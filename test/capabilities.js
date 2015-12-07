b72afcf841cbea693deb22c7915e2ed3e1d60b6a
Dawson Toth <dtoth@appcelerator.com>
2015-12-07 13:46:54 -0500

    [API-1150] Add Coverage

--- test/capabilities.js
+++ test/capabilities.js
@@ -1,6 +1,7 @@
 var should = require('should'),
-	Arrow = require('arrow'),
-	server = new Arrow(),
-	connector = server.getConnector('appc.rss');
+	base = require('./_base'),
+	Arrow = base.Arrow,
+	server = base.server,
+	connector = base.connector;
 
 describe('Connector Capabilities', Arrow.Connector.generateTests(connector, module));
