91ee9f471b183b5739ca47ec5a74af83f87dacbc
Viktor Chobanov <chobanov10100@mail.bg>
2017-06-08 17:32:52 +0300

    [ARRSOF-276] Changed test connector

--- test/_base.js
+++ test/_base.js
@@ -1,7 +1,7 @@
 var should = require('should'),
 	Arrow = require('arrow'),
 	server = new Arrow(),
-	connector = server.getConnector('appc.mysql');
+	connector = server.getConnector('appc.rss');
 
 exports.Arrow = Arrow;
 exports.server = server;
