c12d5147725e015beab6a61f8a59e9e666bb7faf
Martin Marinov <mamarinov@axway.com>
2017-12-14 18:50:20 +0200

    Bump up Grunt version

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-mocha-istanbul')
   grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt')
 
 	// Register tasks.
   grunt.registerTask('default', ['mocha_istanbul:coverage', 'clean'])
